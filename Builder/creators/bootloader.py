45d36511902656c9dae053bb0d46409525d0cc94
tuxlin1204 <tuxlin1211@gmail.com>
2026-01-25 18:07:07 +0300

    grub_install_7

--- Builder/creators/bootloader.py
+++ Builder/creators/bootloader.py
@@ -6,10 +6,9 @@ class GrubTheme:
     def install():
         Logger.add_record("[+] Installing GRUB theme", LoggerStatus.SUCCESS)
         os.system("git clone https://github.com/catppuccin/grub.git /tmp/grub")
-        os.system("cp -r /tmp/grub/src/* /usr/share/grub/themes/")
+        os.system("sudo cp -r /tmp/grub/src/* /usr/share/grub/themes/")
         os.system(
-            'grep -q "^GRUB_THEME=" /etc/default/grub || '
-            'echo \'GRUB_THEME="/usr/share/grub/themes/catppuccin-mocha-grub-theme/theme.txt"\' | sudo tee -a /etc/default/grub'
+            'echo \'GRUB_THEME="/usr/share/grub/themes/catppuccin-mocha-grub-theme/theme.txt"\' | sudo tee /etc/default/grub'
         )
         os.system("sudo grub-mkconfig -o /boot/grub/grub.cfg")
 
