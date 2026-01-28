6e969fa0fb6b199cda26599ffbed50dcfb7afcd9
tuxlin1204 <tuxlin1211@gmail.com>
2026-01-28 19:50:47 +0300

    fix sddm_2

--- Builder/creators/sddm.py
+++ Builder/creators/sddm.py
@@ -1,53 +1,12 @@
 import os
-import shutil
-import subprocess
-from pathlib import Path
 
-class SddmTheme:
-    THEME_REPO = "https://github.com/keyitdev/sddm-astronaut-theme.git"
-    THEME_DIR = Path("/usr/share/sddm/themes/sddm-astronaut-theme")
-    CONF_DIR = Path("/etc/sddm.conf.d")
-
-    @staticmethod
-    def run(cmd):
-        print(f"[+] {cmd}")
-        subprocess.run(cmd, shell=True, check=True)
-
+class GrubTheme:
     @staticmethod
     def build():
-        if os.geteuid() != 0:
-            raise SystemExit("❌ Run installer as root")
 
-        # 1. Dependencies
-        SddmTheme.run(
-            "pacman -S --noconfirm "
-            "sddm qt6-svg qt6-virtualkeyboard qt6-multimedia-ffmpeg"
-        )
-
-        # 2. Theme
-        if SddmTheme.THEME_DIR.exists():
-            shutil.rmtree(SddmTheme.THEME_DIR)
-
-        SddmTheme.run(
-            f"git clone --depth 1 {SddmTheme.THEME_REPO} {SddmTheme.THEME_DIR}"
-        )
-
-        # 3. Fonts
-        fonts = SddmTheme.THEME_DIR / "Fonts"
-        if fonts.exists():
-            SddmTheme.run(f"cp -r {fonts}/* /usr/share/fonts/")
-            SddmTheme.run("fc-cache -fv")
-
-        # 4. Configs
-        SddmTheme.CONF_DIR.mkdir(parents=True, exist_ok=True)
-
-        (SddmTheme.CONF_DIR / "theme.conf").write_text(
-            "[Theme]\n"
-            "Current=sddm-astronaut-theme\n"
-        )
-
-        (SddmTheme.CONF_DIR / "virtualkbd.conf").write_text(
-            "[General]\n"
-            "InputMethod=qtvirtualkeyboard\n"
-        )
-
+ os.system("sudo pacman -S --noconfirm sddm qt6-svg qt6-virtualkeyboard qt6-multimedia-ffmpeg") # зависимости для sddm 
+ os.system("sudo git clone -b master --depth 1 https://github.com/keyitdev/sddm-astronaut-theme.git /usr/share/sddm/themes/sddm-astronaut-theme") 
+ os.system("sudo cp -r /usr/share/sddm/themes/sddm-astronaut-theme/Fonts/* /usr/share/fonts/") 
+ os.system('echo \'[Theme]\nCurrent=sddm-astronaut-theme\' | sudo tee /etc/sddm.conf > /dev/null') 
+ 
+ os.system('echo \'[General]\nInputMethod=qtvirtualkeyboard\' | sudo tee /etc/sddm.conf.d/virtualkbd.conf > /dev/null')
