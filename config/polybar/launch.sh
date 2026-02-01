7be50ece206b4eb61d22351d255c34e1b00ee9e7
tuxlin1204 <tuxlin1211@gmail.com>
2026-02-01 17:39:45 +0300

    update_modules

--- config/polybar/launch.sh
+++ config/polybar/launch.sh
@@ -3,6 +3,35 @@
 # Terminate already running bar instances
 killall -q polybar
 
+# Auto-detect backlight
+export POLYBAR_BACKLIGHT=$(
+  for d in /sys/class/backlight/*; do
+    [ -f "$d/max_brightness" ] || continue
+    echo "$(cat "$d/max_brightness") $(basename "$d")"
+  done 2>/dev/null | sort -nr | head -n1 | awk '{print $2}'
+)
+
+# (опционально) если backlight не найден — лог
+if [[ -z "$POLYBAR_BACKLIGHT" ]]; then
+  echo "No backlight detected" >> /tmp/polybar1.log
+fi
+
+# Auto-detect battery
+export POLYBAR_BATTERY=$(ls /sys/class/power_supply 2>/dev/null | grep -E '^BAT' | head -n 1)
+
+# (опционально) если батарея не найдена — лог
+if [[ -z "$POLYBAR_BATTERY" ]]; then
+  echo "No battery detected" >> /tmp/polybar1.log
+fi
+
+# Launch bar1 and bar2
+echo "---" | tee -a /tmp/polybar1.log /tmp/polybar2.log
+
+
+# Launch bar1 and bar2
+echo "---" | tee -a /tmp/polybar1.log /tmp/polybar2.log
+
+
 # Launch bar1 and bar2
 echo "---" | tee -a /tmp/polybar1.log /tmp/polybar2.log
 
